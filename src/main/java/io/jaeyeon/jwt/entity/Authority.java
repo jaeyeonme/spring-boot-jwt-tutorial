4e308cfb0b161a2fe05a8e30bb6057534efe9fd1
jaeyeonme <cjyeon1022@gmail.com>
2022-05-05 23:59:11 +0900

    2022-05-05 Entity

---
+++ src/main/java/io/jaeyeon/jwt/entity/Authority.java
@@ -0,0 +1,6 @@
+package io.jaeyeon.jwt.entity;
+
+public enum Authority {
+
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/io/jaeyeon/jwt/entity/Member.java
@@ -0,0 +1,33 @@
+package io.jaeyeon.jwt.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public Member(String email, String password, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.authority = authority;
+    }
+}
